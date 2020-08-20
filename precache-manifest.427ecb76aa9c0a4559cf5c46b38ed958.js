0fc8edd50af2ea9d51a0ca1cb00928d46c7fdaa1
AjithArun2001 <ajitharu201@gmail.com>
2020-08-20 16:53:10 +0530

    Updates

---
+++ precache-manifest.427ecb76aa9c0a4559cf5c46b38ed958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfa4bcad454a60102813b5f9cc9c965",
+    "url": "/Redux-Example/index.html"
+  },
+  {
+    "revision": "6b1d40a6aa624dac45b7",
+    "url": "/Redux-Example/static/css/main.c9b7e7ef.chunk.css"
+  },
+  {
+    "revision": "afd2bcbf000778ea2d13",
+    "url": "/Redux-Example/static/js/2.0e7a3510.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-Example/static/js/2.0e7a3510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1d40a6aa624dac45b7",
+    "url": "/Redux-Example/static/js/main.89e313c4.chunk.js"
+  },
+  {
+    "revision": "c7bdf4aafe6543713b6f",
+    "url": "/Redux-Example/static/js/runtime-main.618eba7e.js"
+  }
+]);
